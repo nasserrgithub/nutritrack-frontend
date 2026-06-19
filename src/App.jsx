461ebe6ab8f7e2b676daf4885089889e06cc5f4a
nasserrgithub <nasserr.corcuera@.com>
2026-06-19 13:43:04 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-gray-800">NutriTrack</h1>
+    </div>
+  )
+}
+
+export default App
